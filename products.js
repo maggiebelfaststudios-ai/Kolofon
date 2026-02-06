11bc3f4ddf126bf9dec4040b4e15ff0aae242152
Simon Samuelsen <simonlsamuelsen@gmail.com>
2026-02-06 07:55:13 +0100

    Add search bar and fix admin panel

--- products.js
+++ products.js
@@ -1,30 +1,15 @@
 const PRODUCT_CATALOG = [
     {
-        "id": 1770327801435,
-        "created_at": "2026-02-05T21:43:21.534224+00:00",
-        "title": "Crime and Punishment",
-        "price": "1500",
+        "id": 1770360034350,
+        "created_at": "2026-02-06T06:40:34.584963+00:00",
+        "title": "Intention ",
+        "price": "500",
         "stockQuantity": 1,
-        "src": "https://jxprvsfrsnikzkophoty.supabase.co/storage/v1/object/public/models/1770327800665_crime_and_punishment.glb",
-        "author": "Fjodor Dostojevskij",
-        "material": "Akryl",
+        "src": "https://jxprvsfrsnikzkophoty.supabase.co/storage/v1/object/public/models/1770360033517_intuition.glb",
+        "author": "Somebody",
+        "material": "metal",
         "dimentions": null,
-        "description": "En dekonstruktion af Dostojevskijs store værk, der skildrer spændingsfeltet mellem det intellektuelle hovmod og samvittigheden.",
-        "dimensions": "40 x 60 cm",
-        "dimentioner": null
-    },
-    {
-        "id": 1770327847892,
-        "created_at": "2026-02-05T21:44:08.063578+00:00",
-        "title": "1984",
-        "price": "1500",
-        "stockQuantity": 1,
-        "src": "https://jxprvsfrsnikzkophoty.supabase.co/storage/v1/object/public/models/1770327847034_1984.glb",
-        "author": "Geroge Orwell",
-        "material": "Akryl",
-        "dimentions": null,
-        "description": "En dekonstruktion af Dostojevskijs store værk, der skildrer spændingsfeltet mellem det intellektuelle hovmod og samvittigheden.",
-        "dimensions": "40 x 60 cm",
-        "dimentioner": null
+        "description": "bla bla",
+        "dimensions": "550 x 349 cm"
     }
 ];
